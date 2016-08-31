0c64a736b9ae8202793a09fd93318b81c43c0324
Rafael Soto <rooksoto@gmail.com>
2016-08-30 20:19:04 -0400

    Complete

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,8 +1,14 @@
 package nyc.c4q;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        game();
+    }
+
+    public static void game(){
         Hangman hangman = new Hangman();
         while (hangman.getMisses() < 5){
             hangman.printCurrentWord();
@@ -20,5 +26,41 @@ public class Main {
         } else {
             System.out.println("The answer was " + hangman.getSecretWord());
         }
+
+        endGame();
+    }
+
+    public static void endGame () {
+        Scanner input = new Scanner(System.in);
+        boolean validChoice;
+
+        do {
+            System.out.println("***********");
+            System.out.println("Would you like to play again?");
+            System.out.println("[Yes/No]");
+            System.out.println("***********");
+            String choice = input.next();
+
+            switch (choice.toLowerCase()) {
+                case "yes":case "y":
+                    validChoice = true;
+                    System.out.println("***********");
+                    System.out.println("Great! Let's play.");
+                    System.out.println("***********\n");
+                    game();
+                    break;
+                case "no":case "n":
+                    validChoice = true;
+                    System.out.println("***********");
+                    System.out.println("Thanks for playing!");
+                    System.out.println("***********\n");
+                    break;
+                default:
+                    validChoice = false;
+                    System.out.println("***********");
+                    System.out.println("Sorry. I didn't understand.");
+                    continue;
+            }
+        } while (!validChoice);
     }
 }
